c0a411ae9b07c8ef8f08c882a3f2c3b64cfef66f
vancouver29 <vancouver2990@gmail.com>
2022-03-25 16:55:43 +0100

    Making a list routable

---
+++ metro.config.js
@@ -0,0 +1,33 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const defaultSourceExts =
+  require('metro-config/src/defaults/defaults').sourceExts;
+module.exports = {
+  transformer: {
+    getTransformOptions: () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    sourceExts: process.env.RN_SRC_EXT
+      ? [...process.env.RN_SRC_EXT.split(',').concat(defaultSourceExts), 'cjs'] // <-- cjs added here
+      : [...defaultSourceExts, 'cjs'], // <-- cjs added here
+  },
+};
+
+// const { getDefaultConfig } = require('@expo/metro-config');
+
+// const config = getDefaultConfig(__dirname);
+
+// config.resolver.sourceExts = process.env.RN_SRC_EXT
+//     ? [...process.env.RN_SRC_EXT.split(',').concat(config.resolver.sourceExts), 'cjs'] // <-- cjs added here
+//     : [...config.resolver.sourceExts, 'cjs'] // <-- cjs added here
+
+// module.exports = config;

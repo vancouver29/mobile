64c0c9665010c78a9e4c39cc32d94b936325185c
vancouver29 <vancouver2990@gmail.com>
2022-03-25 16:12:34 +0100

    Making a List routable

--- src/screens/note.js
+++ src/screens/note.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const NoteScreen = () => {
+const NoteScreen = (props) => {
+  const id = props.navigation.getParam('id');
+
   return (
     <View style={{ pading: 10 }}>
-      <Text>This is a Note!</Text>
+      <Text>This is a Note ID {id}</Text>
     </View>
   );
 };
